cc274797738ed46b39af284ee0a2aaadc9ba61df
Abdaljawad-Ayah <abdaljawad.ayah94@gmail.com>
2022-04-02 03:50:13 +0300

    UPLODED

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin Panel</title>
+
+    <link rel="stylesheet" href="{{ asset('css/libs.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/libsstyle.css') }}">
+
+    @yield('styles')
+
+</head>
+<body>
+    <div class="nav-md">
+       <div class="container body">
+           <div class="main-container">
+               hello
+           </div>
+       </div>
+    </div>
+</body>
+</html>
